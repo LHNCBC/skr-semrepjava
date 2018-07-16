d89e9e398ff04d1e19181cf4450546dcd7aed62d
dongwookshin <dongwookshin@hotmail.com>
2018-07-16 12:28:05 -0400

    try to resolve strange conflict

--- src/main/java/gov/nih/nlm/semrepjava/core/ChunkedSentence.java
+++ src/main/java/gov/nih/nlm/semrepjava/core/ChunkedSentence.java
@@ -5,25 +5,25 @@ import java.util.List;
 import gov.nih.nlm.ling.core.Sentence;
 import gov.nih.nlm.ling.core.Span;
 
-public class ChunkedSentence extends Sentence{
+public class ChunkedSentence extends Sentence {
 
-	List<Chunk> chunks;
-	
-	public ChunkedSentence(String id, String text, Span span) {
-		super(id, text, span);
-	}
-	
-	public ChunkedSentence(String id, String text, Span span, List<Chunk> chunks) {
-		super(id,text,span);
-		this.chunks = chunks;
-	}
-	
-	public void setChunks(List<Chunk> chunks) {
-		this.chunks = chunks;
-	}
-	
-	public List<Chunk> getChunks() {
-		return this.chunks;
-	}
+    List<Chunk> chunks;
+
+    public ChunkedSentence(String id, String text, Span span) {
+	super(id, text, span);
+    }
+
+    public ChunkedSentence(String id, String text, Span span, List<Chunk> chunks) {
+	super(id, text, span);
+	this.chunks = chunks;
+    }
+
+    public void setChunks(List<Chunk> chunks) {
+	this.chunks = chunks;
+    }
+
+    public List<Chunk> getChunks() {
+	return this.chunks;
+    }
 
 }
