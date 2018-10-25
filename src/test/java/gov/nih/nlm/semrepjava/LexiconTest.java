d692f3ca02d6a62586647dd72db8cb2387bc9fa4
dongwookshin <dongwookshin@hotmail.com>
2018-10-25 14:39:07 -0400

    LexiconWrapper returns LexiconObject which has three objects: (1) Vector<LexRecords> lexRecords: a vector of LexRecord which is class representing lexicon info (2) String matchedString: substring of input that matches the output lexicon (3) String remainingString: the remainder of input string subtracting the matched one.

--- src/test/java/gov/nih/nlm/semrepjava/LexiconTest.java
+++ src/test/java/gov/nih/nlm/semrepjava/LexiconTest.java
@@ -2,13 +2,23 @@ package gov.nih.nlm.semrepjava;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Vector;
+import java.util.logging.Logger;
 
 import gov.nih.nlm.nls.lexAccess.Api.LexAccessApi;
-import gov.nih.nlm.nls.lexAccess.Api.LexAccessApiResult;
+import gov.nih.nlm.nls.lexCheck.Lib.LexRecord;
+import gov.nih.nlm.semrep.utils.LexiconOutput;
 import gov.nih.nlm.semrep.utils.LexiconWrapper;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+/**
+ * Unit test for the LexiconWrapper.
+ * 
+ * @author Dongwook Shin
+ *
+ */
 
 public class LexiconTest extends TestCase {
     /**
@@ -17,6 +27,8 @@ public class LexiconTest extends TestCase {
      * @param testName
      *            name of the test case
      */
+    private static Logger log = Logger.getLogger(LexiconTest.class.getName());
+
     public LexiconTest(String testName) {
 	super(testName);
 
@@ -35,9 +47,18 @@ public class LexiconTest extends TestCase {
      * @throws IOException
      */
     public void testLexicon() throws IOException, SQLException {
+
 	LexiconWrapper lexWrapper = LexiconWrapper.getInstance();
 	LexAccessApi lexAccess = lexWrapper.initializeLexicon("lexAccess.properties");
-	LexAccessApiResult result = lexWrapper.getLexResult("cold");
-	System.out.println(result.GetText());
+	// LexiconOutput result = lexWrapper.findLexicon("breast cancer is curable");
+	// LexiconOutput result = lexWrapper.findLexicon("cancer is treatable.");
+	LexiconOutput result = lexWrapper.findLexicon("treat");
+	Vector<LexRecord> lexRecords = result.getLexRecords();
+	for (LexRecord lexRecord : lexRecords) {
+	    log.info("base: " + lexRecord.GetBase() + ", category: " + lexRecord.GetCategory() + "variant: "
+		    + lexRecord.GetVariants());
+	}
+
+	assertTrue(lexRecords.size() == 2);
     }
 }
