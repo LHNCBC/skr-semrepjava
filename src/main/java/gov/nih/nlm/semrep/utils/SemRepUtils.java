46d0b0a065db0c092ef7beb185b19a3d29388442
dongwookshin <dongwookshin@hotmail.com>
2018-10-25 15:46:27 -0400

    Fix a bug that GNormPlusClient is hung when no result is returned.

--- src/main/java/gov/nih/nlm/semrep/utils/SemRepUtils.java
+++ src/main/java/gov/nih/nlm/semrep/utils/SemRepUtils.java
@@ -10,82 +10,86 @@ import java.net.UnknownHostException;
 import java.util.logging.Logger;
 
 /**
- * A class that contains methods broadly useful in SemRep. 
+ * A class that contains methods broadly useful in SemRep.
  * 
  * @author Halil Kilicoglu
  *
  */
 
 public class SemRepUtils {
-	private static Logger log = Logger.getLogger(SemRepUtils.class.getName());	
+    private static Logger log = Logger.getLogger(SemRepUtils.class.getName());
 
-	/**
-	 * Obtains a socket to connect to a server on a given port.
-	 * It will return null if a socket cannot be obtained.
-	 * 
-	 * @param serverName	name of the server
-	 * @param serverPort		port to connect 
-	 * @return a <code>Socket</code> object for connection
-	 */
-	public static Socket getSocket(String serverName, int serverPort) {
-		Socket s = null;
-		try {
-			s = new Socket(serverName, serverPort);
-		} 
-		catch (UnknownHostException uhe) {
-			log.warning("Unable to bind socket to server at " + serverName + ":" + serverPort + ".");
-			uhe.printStackTrace();
-		} 
-		catch (IOException ioe) {
-			log.warning("General IO error at creating socket to server at " + serverName + ":" + serverPort + ".");
-			ioe.printStackTrace();
-		}
-		return s;
+    /**
+     * Obtains a socket to connect to a server on a given port. It will return null
+     * if a socket cannot be obtained.
+     * 
+     * @param serverName
+     *            name of the server
+     * @param serverPort
+     *            port to connect
+     * @return a <code>Socket</code> object for connection
+     */
+    public static Socket getSocket(String serverName, int serverPort) {
+	Socket s = null;
+	try {
+	    s = new Socket(serverName, serverPort);
+	} catch (UnknownHostException uhe) {
+	    log.warning("Unable to bind socket to server at " + serverName + ":" + serverPort + ".");
+	    uhe.printStackTrace();
+	} catch (IOException ioe) {
+	    log.warning("General IO error at creating socket to server at " + serverName + ":" + serverPort + ".");
+	    ioe.printStackTrace();
 	}
-	
-	/**
-	 * Closes a socket gracefully.
-	 * 
-	 * @param socket		Socket object to close
-	 */
-	public static void closeSocket(Socket socket) {
-		try {
-			socket.close();
-		} catch (IOException ioe) {
-			log.warning("Failed to close socket to " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
-			ioe.printStackTrace();
-		}
+	return s;
+    }
+
+    /**
+     * Closes a socket gracefully.
+     * 
+     * @param socket
+     *            Socket object to close
+     */
+    public static void closeSocket(Socket socket) {
+	try {
+	    socket.close();
+	} catch (IOException ioe) {
+	    log.warning(
+		    "Failed to close socket to " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort());
+	    ioe.printStackTrace();
 	}
-	
-	/**
-	 * Queries a server with the given socket and input string.
-	 * It will return an empty string if a socket problem is encountered.
-	 * 
-	 * @param socket the socket connected with the the  server
-	 * @param input string to be processed by the server process
-	 * @return string returned by server process
-	 */
-	public static String queryServer(Socket socket,String input) {
-		StringBuilder sb = new StringBuilder();  
-		try {
-			// write text to the socket
-			DataInputStream bis = new DataInputStream(socket.getInputStream());
-			BufferedReader br = new BufferedReader(new InputStreamReader(bis));
-			PrintWriter bw = new PrintWriter(socket.getOutputStream(), true);
-			bw.println(input);
-			bw.flush();
-			String line = br.readLine();
-			do {
-				sb.append(line);
-				line = br.readLine();
-			} while(line != null && line.isEmpty() == false);
-			bis.close();
-			br.close();
-		} catch (IOException ioe) {
-			log.warning("Socket I/O error: " + socket.getInetAddress().getHostName() + ":" + socket.getPort());
-			ioe.printStackTrace();
-		}
-		return sb.toString();
+    }
+
+    /**
+     * Queries a server with the given socket and input string. It will return an
+     * empty string if a socket problem is encountered.
+     * 
+     * @param socket
+     *            the socket connected with the the server
+     * @param input
+     *            string to be processed by the server process
+     * @return string returned by server process
+     */
+    public static String queryServer(Socket socket, String input) {
+	StringBuilder sb = new StringBuilder();
+	try {
+	    // write text to the socket
+	    DataInputStream bis = new DataInputStream(socket.getInputStream());
+	    BufferedReader br = new BufferedReader(new InputStreamReader(bis));
+	    PrintWriter bw = new PrintWriter(socket.getOutputStream(), true);
+	    bw.println(input);
+	    bw.flush();
+	    String line = null;
+	    do {
+		line = br.readLine();
+		sb.append(line);
+	    } while (line != null && line.isEmpty() == false);
+	    bis.close();
+	    br.close();
+	} catch (IOException ioe) {
+	    log.warning("Socket I/O error: " + socket.getInetAddress().getHostName() + ":" + socket.getPort());
+	    ioe.printStackTrace();
 	}
-	
+	return sb.toString();
+    }
+
 }
--- src/test/java/gov/nih/nlm/ner/gnormplus/GNormPlusClientTest.java
+++ src/test/java/gov/nih/nlm/ner/gnormplus/GNormPlusClientTest.java
@@ -10,7 +10,6 @@ import java.util.logging.Logger;
 
 import gov.nih.nlm.ling.core.Document;
 import gov.nih.nlm.ling.core.SpanList;
-import gov.nih.nlm.ling.sem.Concept;
 import gov.nih.nlm.ling.sem.Ontology;
 import gov.nih.nlm.ling.util.FileUtils;
 import gov.nih.nlm.semrep.SemRep;
@@ -19,56 +18,55 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for  the GNormPlus client.
+ * Unit test for the GNormPlus client.
  * 
  * @author Halil Kilicoglu
  *
  */
 
-public class GNormPlusClientTest extends TestCase
-{
-	private static Logger log = Logger.getLogger(GNormPlusClientTest.class.getName());	
+public class GNormPlusClientTest extends TestCase {
+    private static Logger log = Logger.getLogger(GNormPlusClientTest.class.getName());
+
     /**
      * Create the test case
      *
-     * @param testName name of the test case
+     * @param testName
+     *            name of the test case
      */
-    public GNormPlusClientTest(String testName)
-    {
-        super( testName );
-     }
+    public GNormPlusClientTest(String testName) {
+	super(testName);
+    }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( GNormPlusClientTest.class );
+    public static Test suite() {
+	return new TestSuite(GNormPlusClientTest.class);
     }
 
     /**
      * Simple test for GNormPlus
      * 
-     * @throws IOException 
+     * @throws IOException
      */
-    public void testGNormPlusClient() throws IOException
-    {
-    	SemRep.initLogging();
-    	Properties props = FileUtils.loadPropertiesFromFile("semrepjava.properties");
-		Document doc = SemRep.lexicoSyntacticAnalysis("0", "BRCA1");
-		Map<SpanList, LinkedHashSet<Ontology>> annotations = new HashMap<SpanList, LinkedHashSet<Ontology>>();
-    	GNormPlusClient gnormplus = new GNormPlusClient(props);
-    	gnormplus.annotate(doc, props, annotations);
-    	Set<SpanList> set = annotations.keySet();
-    	for (SpanList s1 : set) {
-    	    Set<Ontology> onts = annotations.get(s1);
-    	    for (Ontology ont : onts) {
-    	    	if (ont instanceof GNormPlusConcept) {
-    	    		GNormPlusConcept gpc = (GNormPlusConcept) ont;
-    	    		log.info("Concept: "  + s1.toString() + "\t" + gpc.toString());
-    	    	}
-    		}    
-    	}
-        assertTrue( annotations.size() != 0 );
+    public void testGNormPlusClient() throws IOException {
+	SemRep.initLogging();
+	Properties props = FileUtils.loadPropertiesFromFile("semrepjava.properties");
+	// Document doc = SemRep.lexicoSyntacticAnalysis("0", "BRCA1");
+	Document doc = SemRep.lexicoSyntacticAnalysis("0", "ab");
+	Map<SpanList, LinkedHashSet<Ontology>> annotations = new HashMap<>();
+	GNormPlusClient gnormplus = new GNormPlusClient(props);
+	gnormplus.annotate(doc, props, annotations);
+	Set<SpanList> set = annotations.keySet();
+	for (SpanList s1 : set) {
+	    Set<Ontology> onts = annotations.get(s1);
+	    for (Ontology ont : onts) {
+		if (ont instanceof GNormPlusConcept) {
+		    GNormPlusConcept gpc = (GNormPlusConcept) ont;
+		    log.info("Concept: " + s1.toString() + "\t" + gpc.toString());
+		}
+	    }
+	}
+	assertTrue(annotations.size() != 0);
     }
 }
